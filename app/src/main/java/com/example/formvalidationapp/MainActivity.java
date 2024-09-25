33e14363d04fb3c1963df5b3940f3c413b3e05df
krygsm <lodomirfiol@gmail.com>
2024-09-25 10:22:26 +0200

    UPDATE: Validation message priority, surname form added

--- app/src/main/java/com/example/formvalidationapp/MainActivity.java
+++ app/src/main/java/com/example/formvalidationapp/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity
 {
     private EditText nameInputForm;
+    private EditText surnameInputForm;
     private EditText mailInputForm;
     private EditText numberInputForm;
     private EditText passwordInputForm;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity
 
 
         nameInputForm = findViewById(R.id.nameForm);
+        surnameInputForm = findViewById(R.id.surnameForm);
         mailInputForm = findViewById(R.id.mailForm);
         numberInputForm = findViewById(R.id.numberForm);
         passwordInputForm = findViewById(R.id.passwordForm);
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 String name = nameInputForm.getText().toString().trim();
+                String surname = surnameInputForm.getText().toString().trim();
                 String mail = mailInputForm.getText().toString().trim();
                 String number = numberInputForm.getText().toString().trim();
                 String password = passwordInputForm.getText().toString().trim();
@@ -46,15 +49,15 @@ public class MainActivity extends AppCompatActivity
 
                 if(!password.isEmpty() && !confirmation.isEmpty() && !password.equals(confirmation)) { Toast.makeText(MainActivity.this,  "Hasła się nie zgadzają", Toast.LENGTH_SHORT).show(); return; }
                 if(!mail.isEmpty() && (!Patterns.EMAIL_ADDRESS.matcher(mail).matches())) { Toast.makeText(MainActivity.this,  "Niepoprawny adres e-mail", Toast.LENGTH_SHORT).show(); return; }
+                if(!number.isEmpty() && number.length() < 9) { Toast.makeText(MainActivity.this,  "Nuemr telefonu powinien mieć min. 9 znaków", Toast.LENGTH_SHORT).show(); return; }
+                if(!password.isEmpty() && password.length() < 6) { Toast.makeText(MainActivity.this,  "Hasło powinno mieć min. 6 znaków", Toast.LENGTH_SHORT).show(); return; }
 
                 if(name.isEmpty()) { Toast.makeText(MainActivity.this,  "Wprowadź imię", Toast.LENGTH_SHORT).show();  return; }
+                if(surname.isEmpty()) { Toast.makeText(MainActivity.this,  "Wprowadź nazwisko", Toast.LENGTH_SHORT).show();  return; }
                 if(mail.isEmpty()) { Toast.makeText(MainActivity.this,  "Wprowadź e-mail", Toast.LENGTH_SHORT).show(); return; }
                 if(number.isEmpty()) { Toast.makeText(MainActivity.this,  "Wprowadź numer", Toast.LENGTH_SHORT).show(); return; }
                 if(password.isEmpty()) { Toast.makeText(MainActivity.this,  "Wprowadź hasło", Toast.LENGTH_SHORT).show(); return; }
                 if(confirmation.isEmpty()) { Toast.makeText(MainActivity.this,  "Potwierdź hasło", Toast.LENGTH_SHORT).show(); return; }
-
-                if(number.length() < 9) { Toast.makeText(MainActivity.this,  "Nuemr telefonu powinien mieć min. 9 znaków", Toast.LENGTH_SHORT).show(); return; }
-                if(password.length() < 6) { Toast.makeText(MainActivity.this,  "Hasło powinno mieć min. 6 znaków", Toast.LENGTH_SHORT).show(); return; }
 
                 Toast.makeText(MainActivity.this, "Rejestracja udana", Toast.LENGTH_SHORT).show();
             }
